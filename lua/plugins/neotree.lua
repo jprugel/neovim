fdf3a0cd35753bff47217c54b874f62383e25113
jprugel <jonathon.prugel@gmail.com>
2024-01-16 19:50:54 -0500

    Initial Commit

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.g.neotree_hide_dotfiles = 0
+    vim.keymap.set('n', '<leader>n', ':Neotree<CR>')
+  end
+}
